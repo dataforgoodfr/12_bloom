5bde2e131e35ea283918041418f6bf31c1c24569
Amine Saboni <saboni.amine@gmail.com>
2023-02-05 13:58:25 +0100

    Feat: Clean code & use app in local & remote mode from same implementation

---
+++ app.py
@@ -0,0 +1,48 @@
+import argparse
+from logging import getLogger
+from src.scheduler import PeriodicScheduler
+from src.enums import ExecutionMode
+from src.infra.marine_traffic_scraper import MarineTrafficVesselScraper
+from src.infra.repository_vessel import VesselRepository
+from src.usecase.ScrapVesselsFromMarineTraffic import ScrapVesselsFromMarineTraffic
+
+SCRAP_INTERVAL = 15 * 60
+
+logger = getLogger()
+
+
+def scrap_vessels_with_marine_traffic():
+    vessels_repository = VesselRepository()
+    scraper = MarineTrafficVesselScraper()
+    ScrapVesselsFromMarineTraffic(vessels_repository, scraper).scrap_vessels()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Bloom scraping application"
+    )
+    parser.add_argument(
+            "-m",
+            "--mode",
+            type=ExecutionMode,
+            help="execution mode of the scraper",
+            required=False,
+            default=ExecutionMode.CRONTAB
+        )
+    args = parser.parse_args()
+    if args.mode == ExecutionMode.LOCAL:
+        logger.info("Starting scraping with internal scheduler")
+        scheduler = PeriodicScheduler(
+            function=scrap_vessels_with_marine_traffic,
+            interval=SCRAP_INTERVAL
+        )
+        scrap_vessels_with_marine_traffic()
+        while True:
+            scheduler.start()
+    else:
+        logger.info("Starting scraping with external scheduler")
+        scrap_vessels_with_marine_traffic()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/domain/vessel.py
@@ -0,0 +1,53 @@
+from abc import ABC, abstractmethod
+from datetime import datetime, timezone
+from typing import List, Optional
+
+from pydantic import BaseModel, validator
+
+
+class Vessel(BaseModel):
+    timestamp: Optional[datetime]
+    ship_name: Optional[str]
+    IMO: str
+    last_position_time: Optional[datetime]
+    latitude: Optional[float]
+    longitude: Optional[float]
+
+    def to_list(self):
+        return [
+            self.timestamp,
+            self.ship_name,
+            self.IMO,
+            self.last_position_time,
+            self.latitude,
+            self.longitude
+        ]
+
+    @validator('timestamp', pre=True)
+    def format_timestamp(cls, value):
+        return datetime.strptime(value, '%Y-%m-%d %H:%M UTC').replace(tzinfo=timezone.utc)
+
+    @validator('last_position_time', pre=True)
+    def format_last_position_time(cls, value):
+        if isinstance(value, str):
+            return datetime.strptime(value, '%Y-%m-%d %H:%M UTC').replace(tzinfo=timezone.utc)
+        else:
+            return None
+
+    @validator('latitude', 'longitude', pre=True)
+    def format_coordinates(cls, value):
+        if isinstance(value, str):
+            return float(value)
+        else:
+            return None
+
+
+class AbstractVessel(ABC):
+
+    @abstractmethod
+    def load_vessel_identifiers(self) -> List[Vessel]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def save_vessels(self, vessels_list: List[Vessel]) -> None:
+        raise NotImplementedError
