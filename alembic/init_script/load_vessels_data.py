5c392db7db49afc577cc135c660d427c3e7ca11b
Ronan Sy <ronan_2@hotmail.com>
2023-05-03 21:54:08 +0200

    fix: load vessel script

--- alembic/init_script/load_vessels_data.py
+++ alembic/init_script/load_vessels_data.py
@@ -1,18 +1,3 @@
-"""
-This script presents a method to load geometry data in a local DB.
-First, you will need 4 shape files present in the data directory :
-data/Nonterrestrial_WDPA_Jan2023.dbf
-data/Nonterrestrial_WDPA_Jan2023.prj
-data/Nonterrestrial_WDPA_Jan2023.shp
-data/Nonterrestrial_WDPA_Jan2023.shx
-The, you will have to spawn a database and a pgadmin containers locally,
-using the db.yaml docker compose file.
-#! docker compose up -d postgres pgadmin
-
-Once images are built and running, you can run the following
-python script from the root of the bloom project.
-"""
-
 from pathlib import Path
 
 import pandas as pd
@@ -21,5 +6,8 @@ from sqlalchemy import create_engine
 # The db url is configured with the db connexion variables declared in the db.yaml file.
 db_url = "postgresql://bloom_user:bloom@postgres:5432/bloom_db"
 engine = create_engine(db_url, echo=False)
-df = pd.read_csv(Path.joinpath(Path.cwd(), "data/chalutiers_pelagiques.csv"), sep=";")
+df = pd.read_csv(
+    Path.joinpath(Path.cwd(), "../../data/chalutiers_pelagiques.csv"),
+    sep=";",
+)
 df.to_sql("vessels", engine, if_exists="append", index=False)
