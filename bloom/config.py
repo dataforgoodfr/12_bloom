eb7fdd1b8a5280f72d891b1212d9ee28ca6583a2
Ronan Sy <ronan_2@hotmail.com>
2023-05-08 22:47:05 +0200

    fix: add db_url in Settings.py

--- bloom/config.py
+++ bloom/config.py
@@ -1,11 +1,26 @@
-from pydantic import BaseSettings, Field
+import os
+
+from pydantic import BaseSettings
 
 
 class Settings(BaseSettings):
-    db_url: str = Field(
-        "postgresql://bloom_user:bloom@postgres_bloom:5432/bloom_db",
-        env="DATABASE_URL",
+
+    postrges_user = os.environ.get("POSTGRES_USER")
+    postrges_password = os.environ.get("POSTRGES_PASSWORD")
+    postrges_hostname = os.environ.get("POSTGRES_HOSTNAME")
+    postrges_db = os.environ.get("POSTGRES_DB")
+
+    db_url = (
+        "postgresql://"
+        + postrges_user
+        + ":"
+        + postrges_password
+        + "@"
+        + postrges_hostname
+        + ":5432/"
+        + postrges_db
     )
+
     srid: int = 4236
 
 
