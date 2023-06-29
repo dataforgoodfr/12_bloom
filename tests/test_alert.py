0b07c315ce8121f2cf6b82311c6335236654a131
Ronan Sy <ronan_2@hotmail.com>
2023-06-29 16:02:06 +0200

    fix: try with data out of the scope

--- tests/test_alert.py
+++ tests/test_alert.py
@@ -2,14 +2,21 @@ from container import UseCases
 from bloom.domain.alert import Alert
 from datetime import datetime, timezone
 
+test_alert = Alert(
+    "ship_name",
+    622768943,
+    datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M UTC"),
+    "position",
+    "iucn_cat",
+    "mpa_name",
+)
+
 
 def test_launch_alert():
 
     use_cases = UseCases()
     alert_usecase = use_cases.generate_alert_usecase()
-    timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M UTC")
-    alert = Alert("ship_name", 622768943, timestamp, "position", "iucn_cat", "mpa_name")
     alert_usecase.send_slack_alert(
-        alert,
+        test_alert,
         "THIS IS A TEST",
     )
