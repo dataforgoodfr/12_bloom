6b5c549985c8e3208b96edc5397e27acde889f65
Ronan Sy <ronan_2@hotmail.com>
2023-06-29 15:10:03 +0200

    feat: add tests

---
+++ tests/test_alert.py
@@ -0,0 +1,14 @@
+from container import UseCases
+from bloom.domain.alert import Alert
+from datetime import datetime, timezone
+
+
+def test_driver_tabs_opening():
+
+    use_cases = UseCases()
+    alert_usecase = use_cases.generate_alert_usecase()
+    datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M UTC")
+    alert_usecase.send_slack_alert(
+        Alert("ship_name", 622768943, "position", "iucn_cat", "mpa_name"),
+        "THIS IS A TEST",
+    )
