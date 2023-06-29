8c5f6445899df8a63bc74427d5793aa18af02633
Ronan Sy <ronan_2@hotmail.com>
2023-06-29 15:54:24 +0200

    fix: add init to Alert class

--- bloom/domain/alert.py
+++ bloom/domain/alert.py
@@ -10,3 +10,19 @@ class Alert(BaseModel):
     position: str
     iucn_cat: str
     mpa_name: str
+
+    def __init__(  # noqa: PLR0913
+        self,
+        ship_name: str,
+        mmsi: int,
+        last_position_time: datetime,
+        position: str,
+        iucn_cat: str,
+        mpa_name: str,
+    ) -> None:
+        self.ship_name = ship_name
+        self.mmsi = mmsi
+        self.last_position_time = last_position_time
+        self.position = position
+        self.iucn_cat = iucn_cat
+        self.mpa_name = mpa_name
