7ab6aec6b623d9e374b49a9fc0d0e092e2676f0a
Ronan Sy <ronan_2@hotmail.com>
2023-06-29 15:52:07 +0200

    fix: timestamp missing

--- tests/test_alert.py
+++ tests/test_alert.py
@@ -7,8 +7,8 @@ def test_launch_alert():
 
     use_cases = UseCases()
     alert_usecase = use_cases.generate_alert_usecase()
-    datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M UTC")
-    alert = Alert("ship_name", 622768943, "position", "iucn_cat", "mpa_name")
+    timestamp = datetime.now(timezone.utc).strftime("%Y-%m-%d %H:%M UTC")
+    alert = Alert("ship_name", 622768943, timestamp, "position", "iucn_cat", "mpa_name")
     alert_usecase.send_slack_alert(
         alert,
         "THIS IS A TEST",
