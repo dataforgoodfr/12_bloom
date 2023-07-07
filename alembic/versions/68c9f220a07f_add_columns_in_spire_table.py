61a208157ac8d44b128c8ccb2f679eda613ede25
Ronan Sy <ronan_2@hotmail.com>
2023-07-07 11:44:37 +0200

    feat: add alembic version

--- alembic/versions/68c9f220a07f_add_columns_in_spire_table.py
+++ alembic/versions/68c9f220a07f_add_columns_in_spire_table.py
@@ -5,7 +5,9 @@ Revises: 1fd83d22bd1e
 Create Date: 2023-07-07 11:15:16.516122
 
 """
+import sqlalchemy as sa
 
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision = "68c9f220a07f"
@@ -15,12 +17,36 @@ depends_on = None
 
 
 def upgrade() -> None:
-    pass
-
-    # taille : VesselStaticData -> VesselDimensions -> length, width (Int) ,
-    # voyage (destination: String , draught: Float eta: DateTime)
-    # rm : fishing, at_port, port_name, status
+    with op.batch_alter_table("spire_vessel_positions") as batch_op:
+        batch_op.add_column(sa.Column("vessel_length", sa.Integer))
+        batch_op.add_column(sa.Column("vessel_width", sa.Integer))
+        batch_op.add_column(sa.Column("voyage_destination", sa.String))
+        batch_op.add_column(sa.Column("voyage_draught", sa.Float))
+        batch_op.add_column(sa.Column("voyage_eta", sa.DateTime))
+        batch_op.add_column(sa.Column("accuracy", sa.String))
+        batch_op.add_column(sa.Column("position_sensors", sa.String))
+        batch_op.add_column(sa.Column("course", sa.Float))
+        batch_op.add_column(sa.Column("heading", sa.Float))
+        batch_op.add_column(sa.Column("rot", sa.Float))
+        batch_op.drop_column("fishing")
+        batch_op.drop_column("at_port")
+        batch_op.drop_column("port_name")
+        batch_op.drop_column("status")
 
 
 def downgrade() -> None:
-    pass
+    with op.batch_alter_table("spire_vessel_positions") as batch_op:
+        batch_op.drop_column("vessel_length")
+        batch_op.drop_column("vessel_width")
+        batch_op.drop_column("voyage_destination")
+        batch_op.drop_column("voyage_draught")
+        batch_op.drop_column("voyage_eta")
+        batch_op.drop_column("accuracy")
+        batch_op.drop_column("position_sensors")
+        batch_op.drop_column("course")
+        batch_op.drop_column("heading")
+        batch_op.drop_column("rot")
+        batch_op.add_column(sa.Column("fishing", sa.Boolean))
+        batch_op.add_column(sa.Column("at_port", sa.Boolean))
+        batch_op.add_column(sa.Column("port_name", sa.String))
+        batch_op.add_column(sa.Column("status", sa.String))
