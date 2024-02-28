741b94b164b2cb55f518624fdf41a766f207bac1
RV <rvlebars@gmail.com>
2024-02-28 15:33:20 +0100

    fix: cron_scrapper

--- docker-env/entrypoint.sh
+++ docker-env/entrypoint.sh
@@ -5,7 +5,7 @@
 # The entrypoint.sh script stores ENV vars at runtime in the ~/.env file as key=value pairs
 # Then the cron line include some command to load these ENV vars from file before launching app.py
 # This mecanism allows to give access to the same ENV vars for app.py launch in terminal and launch via cron
-env | egrep '^(POSTGRES_.*|SPIRE_TOKEN.*)' > ~/.env
+export PATH_ENV=/tmp/.env.cron;/venv/bin/python3 -c "from bloom.config import settings"
 
 /etc/init.d/rsyslog restart
 
