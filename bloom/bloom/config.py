de03dbecf1afd07c81c1a5445cc0d25be6d8fbb7
RV <rvlebars@gmail.com>
2024-03-01 09:16:16 +0100

    fix: bloom/config.py:13:97: E501 Line too long (130 > 96 characters)

--- bloom/bloom/config.py
+++ bloom/bloom/config.py
@@ -9,19 +9,20 @@ class Settings(BaseSettings):
     postgres_port = os.environ.get("POSTGRES_PORT")
     postgres_db = os.environ.get("POSTGRES_DB")
     
-    print("db_url: ", "postgresql://"+postgres_user+":"+postgres_password+"@"+postgres_hostname+":"+postgres_port+"/"+postgres_db)
+    #print("db_url: ", f"postgresql://{postgres_user}:{postgres_password}@{postgres_hostname}:"
+    #                    f"{postgres_port}/{postgres_db}")
 
     db_url = (
         "postgresql://"
-        + postgres_user
-        + ":"
-        + postgres_password
-        + "@"
-        + postgres_hostname
-        + ":"
-        + postgres_port
-        + "/"
-        + postgres_db
+        f"{postgres_user}"
+        ":"
+        f"{postgres_password}"
+        "@"
+        f"{postgres_hostname}"
+        ":"
+        f"{postgres_port}"
+        "/"
+        f"{postgres_db}"
     )
 
     srid: int = 4326
