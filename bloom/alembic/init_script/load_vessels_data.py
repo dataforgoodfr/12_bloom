2e78ff71047653ca7b79ae5243bacd714d605f25
herve.le-bars <herve.le-bars@aviation-civile.gouv.fr>
2024-03-04 15:05:22 +0100

    fix: conformization of settings acces via bloom.config

--- bloom/alembic/init_script/load_vessels_data.py
+++ bloom/alembic/init_script/load_vessels_data.py
@@ -4,30 +4,12 @@ from pathlib import Path
 
 import pandas as pd
 from sqlalchemy import create_engine
+from bloom.config import settings
 
 logging.basicConfig()
 logging.getLogger("sqlalchemy.engine").setLevel(logging.INFO)
 
-postgres_user = os.environ.get("POSTGRES_USER")
-postgres_password = os.environ.get("POSTGRES_PASSWORD")
-postgres_hostname = os.environ.get("POSTGRES_HOSTNAME")
-postgres_db = os.environ.get("POSTGRES_DB")
-postgres_port = os.environ.get("POSTGRES_PORT")
-
-# The db url is configured with the db connexion variables declared in the db.yaml file.
-db_url = (
-    "postgresql://"
-    + postgres_user
-    + ":"
-    + postgres_password
-    + "@"
-    + postgres_hostname
-    + ":"
-    + postgres_port
-    + "/"
-    + postgres_db
-)
-engine = create_engine(db_url)
+engine = create_engine(settings.db_url)
 df = pd.read_csv(
     Path(__file__).parent.joinpath("../../data/chalutiers_pelagiques.csv"),
     sep=";",
