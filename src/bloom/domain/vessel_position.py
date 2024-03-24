3f142927f1c1a4c163b613d4868c44b21d34f8b3
njouanin <nicolas.jouanin++github@mailo.com>
2024-03-24 14:38:10 +0100

    Compatibilité python + valeurs par défaut

--- src/bloom/domain/vessel_position.py
+++ src/bloom/domain/vessel_position.py
@@ -1,12 +1,14 @@
 from datetime import datetime
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 from shapely import Point
 
 from typing import Union
 
 
 class VesselPosition(BaseModel):
+    model_config = ConfigDict(arbitrary_types_allowed=True)
+
     id: int | None = None
     timestamp: datetime
     accuracy: Union[str, None] = None
@@ -14,11 +16,11 @@ class VesselPosition(BaseModel):
     course: Union[float, None] = None
     heading: Union[float, None] = None
     position: Union[Point, None] = None
-    latitude: float | None
-    longitude: float | None
-    maneuver: str | None
-    navigational_status: str | None
-    rot: float | None
-    speed: float | None
+    latitude: Union[float | None] = None
+    longitude: Union[float | None] = None
+    maneuver: Union[str | None] = None
+    navigational_status: Union[str | None] = None
+    rot: Union[float | None] = None
+    speed: Union[float | None] = None
     vessel_id: int
     created_at: Union[datetime, None] = None
