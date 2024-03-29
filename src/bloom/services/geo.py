77f4e21238e6b29f12764b29fd485c8187364bf6
Hugo Ehlinger <hugoehlinger@laposte.net>
2024-03-29 22:50:59 +0100

    [FIX]: moved script to src/bloom/services/geo.py

---
+++ src/bloom/services/geo.py
@@ -0,0 +1,57 @@
+from typing import List
+import pandas as pd
+import geopandas as gpd
+
+from bloom.container import UseCases
+from bloom.config import settings
+
+def find_positions_in_port_buffer(vessel_positions: List[tuple]) -> List[tuple]:
+    """Assigns vessel positions to their port if any. If a vessel is not in a port,
+    assign np.nan as port_id
+
+    :param List[tuple] vessel_positions: fields "vessel_id", "longitude", "latitude", 
+
+    :return List[tuple] vessel_positions: fields "vessel_id", "longitude", "latitude", "port_name", "port_id"
+        if a vessel is not in a port, "port_id" is np.nan
+    """
+
+    # Convert vessel positions into a GeoDataFrame
+    df_vessel_positions = pd.DataFrame(
+        vessel_positions, columns=["vessel_id", "lon", "lat"]
+    )
+    gdf_vessel_positions = gpd.GeoDataFrame(
+        df_vessel_positions,
+        geometry=gpd.points_from_xy(
+            df_vessel_positions["lon"], df_vessel_positions["lat"]
+        ),
+        crs=settings.srid,
+    )
+
+    # Get all ports from DataBase
+    use_cases = UseCases()
+    port_repository = use_cases.port_repository()
+    db = use_cases.db()
+    with db.session() as session:
+        ports = port_repository.get_all_ports(session)
+        
+    df_ports = pd.DataFrame(
+        [[p.id, p.name, p.geometry_buffer] for p in ports],
+        columns=["port_id", "port_name", "geometry"],
+    )
+    gdf_ports = gpd.GeoDataFrame(df_ports, geometry="geometry", crs=settings.srid)
+
+    # Spatial join to match vessel positions to ports polygons
+    gdf_vessel_positions = gdf_vessel_positions.sjoin(gdf_ports, how="left")
+
+    # Format response as list of tuples with port_id as last element
+    response = list(
+        zip(
+            gdf_vessel_positions["vessel_id"],
+            gdf_vessel_positions["lon"],
+            gdf_vessel_positions["lat"],
+            gdf_vessel_positions["port_id"],
+            gdf_vessel_positions["port_name"],
+        )
+    )
+
+    return response
--- src/tests/test_vessel_is_in_port.py
+++ src/tests/test_vessel_is_in_port.py
@@ -1,18 +1,14 @@
 import math
 
 from bloom.logger import logger
-from bloom.container import UseCases
+from bloom.services import geo
 
 def test_vessel_is_in_port():
-    use_cases = UseCases()
-    port_repository = use_cases.port_repository()
-    db = use_cases.db()
-    with db.session() as session:
-        test_data = [
-            (1, 2.2087008337345244, 51.03849547707603), # Dunkerque
-            (2, 2.3567829215805722, 51.73938862920116), # in the see
-        ]
-        response = port_repository.find_positions_in_port_buffer(session, test_data)
+    test_data = [
+        (1, 2.2087008337345244, 51.03849547707603), # Dunkerque
+        (2, 2.3567829215805722, 51.73938862920116), # in the see
+    ]
+    response = geo.find_positions_in_port_buffer(test_data)
 
     assert response[0][-1] == "Dunkerque"
     assert math.isnan(response[1][-2]) is True
