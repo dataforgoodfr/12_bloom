7148b94f84c28cd28dd70d60d85935bab7518a49
herve.le-bars <herve.le-bars@aviation-civile.gouv.fr>
2024-07-01 20:06:32 +0200

    update: frontend from tanega commit:ab01183

--- frontend/app/map/page.tsx
+++ frontend/app/map/page.tsx
@@ -2,12 +2,29 @@ import LeftPanel from "@/components/core/left-panel"
 import MapControls from "@/components/core/map-controls"
 import DemoMap from "@/components/core/map/main-map"
 import PositionPreview from "@/components/core/map/position-preview"
+import { getVessels, getVesselsLatestPositions } from "@/services/backend-rest-client"
 
-export default function MapPage() {
+async function fetchVessels() {
+  const response = await getVessels();
+  return response.data ?? [];
+}
+
+async function fetchLatestPositions() {
+  // TODO(CT): move this logic within a cron job
+  const response = await getVesselsLatestPositions();
+  return response.data ?? [];
+}
+  
+export default async function MapPage() {
+  const vessels = await fetchVessels();
+  const latestPositions = await fetchLatestPositions();
+
+  // TODO: create new client component dedicated to update store 
+  // -> then Map + LeftPanel can just use storeProvider
   return (
     <>
-      <LeftPanel />
-      <DemoMap />
+      <LeftPanel vessels={vessels} />
+      <DemoMap vesselsPositions={latestPositions} />
       <MapControls />
       <PositionPreview />
     </>
--- frontend/types/vessel.tsx
+++ frontend/types/vessel.tsx
@@ -1,10 +1,54 @@
 export type Vessel = {
-  name: string
+  id: number
   mmsi: number
-  imo: number
+  ship_name: string
   width: number
   length: number
-  flag: string
-  callsign: string
-  ship_type: string | null
+  country_iso3: string
+  type: string | null
+  imo: number | null
+  cfr: string
+  external_marking: string
+  ircs: string
+  length_class: string
+}
+
+export type VesselPositions = VesselPosition[]
+
+export interface VesselPosition {
+  arrival_port: string;
+  excurision_id: number;
+  heading: number;
+  position: VesselPositionCoordinates;
+  speed: number;
+  timestamp: string;
+  vessel: Vessel;
+}
+
+export interface VesselPositionCoordinates {
+  coordinates: number[];
+  type: string;
+}
+
+export type VesselVoyageTracksPropertiesType = {
+  vessel_ais_class: string
+  vessel_flag: string
+  vessel_name: string
+  vessel_callsign: string
+  vessel_ship_type?: string
+  vessel_sub_ship_type?: string
+  vessel_mmsi: number
+  vessel_imo: number
+  vessel_width: number
+  vessel_length: number
+  voyage_destination?: string
+  voyage_draught?: number
+  voyage_eta: string
+}
+
+export type VesselTrailPropertiesType = {
+  vessel_mmsi: number
+  speed: number
+  heading?: number
+  navigational_status: string
 }
