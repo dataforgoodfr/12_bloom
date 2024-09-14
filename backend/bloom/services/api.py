e06edb3c58ab5648e912a49ee063f19fcf4e90da
herve.le-bars <herve.le-bars@aviation-civile.gouv.fr>
2024-09-14 18:31:46 +0200

    feat: add ports & ports/{id} endpoints
    # Conflicts:
    #	backend/bloom/services/api.py

--- backend/bloom/services/api.py
+++ backend/bloom/services/api.py
@@ -8,14 +8,11 @@ import redis
 import json
 from bloom.config import settings
 from bloom.container import UseCases
-from bloom.domain.vessel import Vessel
 from bloom.logger import logger
 
 rd = redis.Redis(host=settings.redis_host, port=settings.redis_port, db=0)
 
-from datetime import datetime
 import time
-
 
 app = FastAPI()
 
@@ -28,30 +25,32 @@ def check_apikey(key:str):
 async def cache_all_flush(request:Request,key: str = Depends(header_scheme)):
     check_apikey(key)
     rd.flushall()
-    return {"code":0}
+    return {"code": 0}
+
 
 @app.get("/vessels")
 async def list_vessels(nocache:bool=False,key: str = Depends(header_scheme)):
     check_apikey(key)
-    endpoint=f"/vessels"
-    cache= rd.get(endpoint)
+    endpoint = f"/vessels"
+    cache = rd.get(endpoint)
     start = time.time()
     if cache and not nocache:
         logger.debug(f"{endpoint} cached ({settings.redis_cache_expiration})s")
-        payload=json.loads(cache)
-        logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+        payload = json.loads(cache)
+        logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
         return payload
     else:
         use_cases = UseCases()
         vessel_repository = use_cases.vessel_repository()
         db = use_cases.db()
         with db.session() as session:
-            
+
             json_data = [json.loads(v.model_dump_json() if v else "{}")
-                            for v in vessel_repository.get_vessels_list(session)]
+                         for v in vessel_repository.get_vessels_list(session)]
             rd.set(endpoint, json.dumps(json_data))
-            rd.expire(endpoint,settings.redis_cache_expiration)
+            rd.expire(endpoint, settings.redis_cache_expiration)
             return json_data
+
 
 @app.get("/vessels/{vessel_id}")
 async def get_vessel(vessel_id: int,key: str = Depends(header_scheme)):
@@ -60,7 +59,8 @@ async def get_vessel(vessel_id: int,key: str = Depends(header_scheme)):
     vessel_repository = use_cases.vessel_repository()
     db = use_cases.db()
     with db.session() as session:
-        return vessel_repository.get_vessel_by_id(session,vessel_id)
+        return vessel_repository.get_vessel_by_id(session, vessel_id)
+
 
 @app.get("/vessels/all/positions/last")
 async def list_all_vessel_last_position(nocache:bool=False,key: str = Depends(header_scheme)):
@@ -70,8 +70,8 @@ async def list_all_vessel_last_position(nocache:bool=False,key: str = Depends(he
     start = time.time()
     if cache and not nocache:
         logger.debug(f"{endpoint} cached ({settings.redis_cache_expiration})s")
-        payload=json.loads(cache)
-        logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+        payload = json.loads(cache)
+        logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
         return payload
     else:
         use_cases = UseCases()
@@ -81,9 +81,10 @@ async def list_all_vessel_last_position(nocache:bool=False,key: str = Depends(he
             json_data = [json.loads(p.model_dump_json() if p else "{}")
                          for p in segment_repository.get_all_vessels_last_position(session)]
             rd.set(endpoint, json.dumps(json_data))
-            rd.expire(endpoint,settings.redis_cache_expiration)
-            logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+            rd.expire(endpoint, settings.redis_cache_expiration)
+            logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
             return json_data
+
 
 @app.get("/vessels/{vessel_id}/positions/last")
 async def get_vessel_last_position(vessel_id: int, nocache:bool=False,key: str = Depends(header_scheme)):
@@ -93,20 +94,21 @@ async def get_vessel_last_position(vessel_id: int, nocache:bool=False,key: str =
     start = time.time()
     if cache and not nocache:
         logger.debug(f"{endpoint} cached ({settings.redis_cache_expiration})s")
-        payload=json.loads(cache)
-        logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+        payload = json.loads(cache)
+        logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
         return payload
     else:
         use_cases = UseCases()
         segment_repository = use_cases.segment_repository()
         db = use_cases.db()
         with db.session() as session:
-            result=segment_repository.get_vessel_last_position(session,vessel_id)
+            result = segment_repository.get_vessel_last_position(session, vessel_id)
             json_data = json.loads(result.model_dump_json() if result else "{}")
             rd.set(endpoint, json.dumps(json_data))
-            rd.expire(endpoint,settings.redis_cache_expiration)
-            logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+            rd.expire(endpoint, settings.redis_cache_expiration)
+            logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
             return json_data
+
 
 @app.get("/vessels/{vessel_id}/excursions")
 async def list_vessel_excursions(vessel_id: int, nocache:bool=False,key: str = Depends(header_scheme)):
@@ -116,8 +118,8 @@ async def list_vessel_excursions(vessel_id: int, nocache:bool=False,key: str = D
     start = time.time()
     if cache and not nocache:
         logger.debug(f"{endpoint} cached ({settings.redis_cache_expiration})s")
-        payload=json.loads(cache)
-        logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+        payload = json.loads(cache)
+        logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
         return payload
     else:
         use_cases = UseCases()
@@ -125,10 +127,10 @@ async def list_vessel_excursions(vessel_id: int, nocache:bool=False,key: str = D
         db = use_cases.db()
         with db.session() as session:
             json_data = [json.loads(p.model_dump_json() if p else "{}")
-                         for p in excursion_repository.get_excursions_by_vessel_id(session,vessel_id)]
+                         for p in excursion_repository.get_excursions_by_vessel_id(session, vessel_id)]
             rd.set(endpoint, json.dumps(json_data))
-            rd.expire(endpoint,settings.redis_cache_expiration)
-            logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+            rd.expire(endpoint, settings.redis_cache_expiration)
+            logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
         return json_data
 
 
@@ -139,7 +141,7 @@ async def get_vessel_excursion(vessel_id: int,excursions_id: int,key: str = Depe
     excursion_repository = use_cases.excursion_repository()
     db = use_cases.db()
     with db.session() as session:
-        return excursion_repository.get_vessel_excursion_by_id(session,vessel_id,excursions_id)
+        return excursion_repository.get_vessel_excursion_by_id(session, vessel_id, excursions_id)
 
 
 @app.get("/vessels/{vessel_id}/excursions/{excursions_id}/segments")
@@ -149,7 +151,8 @@ async def list_vessel_excursion_segments(vessel_id: int,excursions_id: int,key: 
     segment_repository = use_cases.segment_repository()
     db = use_cases.db()
     with db.session() as session:
-        return segment_repository.list_vessel_excursion_segments(session,vessel_id,excursions_id)
+        return segment_repository.list_vessel_excursion_segments(session, vessel_id, excursions_id)
+
 
 @app.get("/vessels/{vessel_id}/excursions/{excursions_id}/segments/{segment_id}")
 async def get_vessel_excursion_segment(vessel_id: int,excursions_id: int, segment_id:int,key: str = Depends(header_scheme)):
@@ -158,7 +161,8 @@ async def get_vessel_excursion_segment(vessel_id: int,excursions_id: int, segmen
     segment_repository = use_cases.segment_repository()
     db = use_cases.db()
     with db.session() as session:
-        return segment_repository.get_vessel_excursion_segment_by_id(session,vessel_id,excursions_id,segment_id)
+        return segment_repository.get_vessel_excursion_segment_by_id(session, vessel_id, excursions_id, segment_id)
+
 
 @app.get("/ports")
 async def list_ports(request:Request,nocache:bool=False,key: str = Depends(header_scheme)):
@@ -182,7 +186,8 @@ async def list_ports(request:Request,nocache:bool=False,key: str = Depends(heade
             rd.expire(endpoint,settings.redis_cache_expiration)
             logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
             return json_data
-    
+
+
 
 @app.get("/ports/{port_id}")
 async def get_port(port_id:int,key: str = Depends(header_scheme)):
@@ -191,7 +196,8 @@ async def get_port(port_id:int,key: str = Depends(header_scheme)):
     port_repository = use_cases.port_repository()
     db = use_cases.db()
     with db.session() as session:
-        return port_repository.get_port_by_id(session,port_id)
+        return port_repository.get_port_by_id(session, port_id)
+
 
 @app.get("/zones")
 async def list_zones(request:Request,nocache:bool=False,key: str = Depends(header_scheme)):
@@ -201,8 +207,8 @@ async def list_zones(request:Request,nocache:bool=False,key: str = Depends(heade
     start = time.time()
     if cache and not nocache:
         logger.debug(f"{endpoint} cached ({settings.redis_cache_expiration})s")
-        payload=json.loads(cache)
-        logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+        payload = json.loads(cache)
+        logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
         return payload
     else:
         use_cases = UseCases()
@@ -212,9 +218,10 @@ async def list_zones(request:Request,nocache:bool=False,key: str = Depends(heade
             json_data = [json.loads(z.model_dump_json() if z else "{}")
                          for z in zone_repository.get_all_zones(session)]
             rd.set(endpoint, json.dumps(json_data))
-            rd.expire(endpoint,settings.redis_cache_expiration)
-            logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+            rd.expire(endpoint, settings.redis_cache_expiration)
+            logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
             return json_data
+
 
 @app.get("/zones/all/categories")
 async def list_zone_categories(request:Request,nocache:bool=False,key: str = Depends(header_scheme)):
@@ -224,20 +231,21 @@ async def list_zone_categories(request:Request,nocache:bool=False,key: str = Dep
     start = time.time()
     if cache and not nocache:
         logger.debug(f"{endpoint} cached ({settings.redis_cache_expiration})s")
-        payload=json.loads(cache)
-        logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+        payload = json.loads(cache)
+        logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
         return payload
     else:
         use_cases = UseCases()
         zone_repository = use_cases.zone_repository()
         db = use_cases.db()
         with db.session() as session:
-            json_data = [json.loads(z.model_dump_json()  if z else "{}")
+            json_data = [json.loads(z.model_dump_json() if z else "{}")
                          for z in zone_repository.get_all_zone_categories(session)]
             rd.set(endpoint, json.dumps(json_data))
-            rd.expire(endpoint,settings.redis_cache_expiration)
-            logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+            rd.expire(endpoint, settings.redis_cache_expiration)
+            logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
             return json_data
+
 
 @app.get("/zones/by-category/{category}/by-sub-category/{sub}")
 async def get_zone_all_by_category(category:str="all",sub:str=None,nocache:bool=False,key: str = Depends(header_scheme)):
@@ -247,8 +255,8 @@ async def get_zone_all_by_category(category:str="all",sub:str=None,nocache:bool=
     start = time.time()
     if cache and not nocache:
         logger.debug(f"{endpoint} cached ({settings.redis_cache_expiration})s")
-        payload=json.loads(cache)
-        logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+        payload = json.loads(cache)
+        logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
         return payload
     else:
         use_cases = UseCases()
@@ -256,11 +264,14 @@ async def get_zone_all_by_category(category:str="all",sub:str=None,nocache:bool=
         db = use_cases.db()
         with db.session() as session:
             json_data = [json.loads(z.model_dump_json() if z else "{}")
-                         for z in zone_repository.get_all_zones_by_category(session,category if category != 'all' else None,sub)]
+                         for z in
+                         zone_repository.get_all_zones_by_category(session, category if category != 'all' else None,
+                                                                   sub)]
             rd.set(endpoint, json.dumps(json_data))
-            rd.expire(endpoint,settings.redis_cache_expiration)
-            logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+            rd.expire(endpoint, settings.redis_cache_expiration)
+            logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
             return json_data
+
 
 @app.get("/zones/by-category/{category}")
 async def get_zone_all_by_category(category:str="all",nocache:bool=False,key: str = Depends(header_scheme)):
@@ -270,8 +281,8 @@ async def get_zone_all_by_category(category:str="all",nocache:bool=False,key: st
     start = time.time()
     if cache and not nocache:
         logger.debug(f"{endpoint} cached ({settings.redis_cache_expiration})s")
-        payload=json.loads(cache)
-        logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+        payload = json.loads(cache)
+        logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
         return payload
     else:
         use_cases = UseCases()
@@ -279,12 +290,14 @@ async def get_zone_all_by_category(category:str="all",nocache:bool=False,key: st
         db = use_cases.db()
         with db.session() as session:
             json_data = [json.loads(z.model_dump_json() if z else "{}")
-                         for z in zone_repository.get_all_zones_by_category(session,category if category != 'all' else None)]
+                         for z in
+                         zone_repository.get_all_zones_by_category(session, category if category != 'all' else None)]
             rd.set(endpoint, json.dumps(json_data))
-            rd.expire(endpoint,settings.redis_cache_expiration)
-            logger.debug(f"{endpoint} elapsed Time: {time.time()-start}")
+            rd.expire(endpoint, settings.redis_cache_expiration)
+            logger.debug(f"{endpoint} elapsed Time: {time.time() - start}")
             return json_data
-        
+
+
 @app.get("/zones/{zones_id}")
 async def get_zone(zones_id:int,key: str = Depends(header_scheme)):
     check_apikey(key)
@@ -292,13 +305,14 @@ async def get_zone(zones_id:int,key: str = Depends(header_scheme)):
     zone_repository = use_cases.zone_repository()
     db = use_cases.db()
     with db.session() as session:
-        return zone_repository.get_zone_by_id(session,zones_id)
+        return zone_repository.get_zone_by_id(session, zones_id)
+
 
 @app.get("/")
-async def root(request:Request):
+async def root(request: Request):
     return {
-            "cache_all_flush": f"{request.url_for('cache_all_flush')}",
-            "ports":    f"{request.url_for('list_ports')}",
-            "vessels":  f"{request.url_for('list_vessels')}",
-            "zones":    f"{request.url_for('list_zones')}",
-            }
+        "cache_all_flush": f"{request.url_for('cache_all_flush')}",
+        "ports": f"{request.url_for('list_ports')}",
+        "vessels": f"{request.url_for('list_vessels')}",
+        "zones": f"{request.url_for('list_zones')}",
+    }
