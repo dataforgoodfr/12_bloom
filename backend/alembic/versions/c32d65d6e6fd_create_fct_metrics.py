9fb76132e95e761c58edb99a1c06c394dd4c54fd
Ewen <ewenjamet73@gmail.com>
2024-10-18 16:39:54 +0200

    Create Alembic migration for fct metrics

---
+++ backend/alembic/versions/c32d65d6e6fd_create_fct_metrics.py
@@ -0,0 +1,24 @@
+"""create fct metrics
+
+Revision ID: c32d65d6e6fd
+Revises: 06bb3c26076f
+Create Date: 2024-10-18 11:13:02.468934
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c32d65d6e6fd'
+down_revision = '06bb3c26076f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
