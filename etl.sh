916ad33489168edf88e6d6edcf6eaed933a2aa6c
marthevienne <123016211+marthevienne@users.noreply.github.com>
2024-11-11 22:04:21 +0100

    add ETL tasks to cron locked by flock

--- etl.sh
+++ etl.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
-python backend/bloom/tasks/load_spire_data_from_api.py && \
-python backend/bloom/tasks/clean_positions.py && \
-python backend/bloom/tasks/create_update_excursions_segments.py
+exec 200>./var/lock/mylockfile
+flock -n 200 || exit 1
+
+/home/bas/venv/bin/python /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/load_spire_data_from_api.py && \
+/home/bas/venv/bin/python /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/clean_positions.py && \
+/home/bas/venv/bin/python /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/create_update_excursions_segments.py
