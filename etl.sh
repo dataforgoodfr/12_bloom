d8715c7da01092cfdbf4f4c6f2782df38057849d
marthevienne <123016211+marthevienne@users.noreply.github.com>
2024-11-13 17:19:24 +0100

    fix flock etl.sh

--- etl.sh
+++ etl.sh
@@ -1,8 +1,6 @@
 #! /bin/sh
 
-exec 200>./var/lock/mylockfile
-flock -n 200 || exit 1
-
 /home/bas/venv/bin/python /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/load_spire_data_from_api.py && \
-/home/bas/venv/bin/python /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/clean_positions.py && \
-/home/bas/venv/bin/python /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/create_update_excursions_segments.py
+flock /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/clean_positions.py --command "
+    /home/bas/venv/bin/python /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/clean_positions.py && 
+    /home/bas/venv/bin/python /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/create_update_excursions_segments.py"
