02878291ca1db80465699b1927131b74bc70c09e
marthevienne <123016211+marthevienne@users.noreply.github.com>
2024-11-14 16:05:01 +0100

    activate venv in etl.sh

--- etl.sh
+++ etl.sh
@@ -1,6 +1,9 @@
 #! /bin/sh
 
-/home/bas/venv/bin/python /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/load_spire_data_from_api.py && \
-flock /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/clean_positions.py --command "
-    /home/bas/venv/bin/python /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/clean_positions.py && 
-    /home/bas/venv/bin/python /home/bas/app_7244f095-70bc-43c3-9950-d075c01af05f/backend/bloom/tasks/create_update_excursions_segments.py"
+source /home/bas/venv/bin/activate
+cd $APP_FOLDER
+
+python bloom/tasks/load_spire_data_from_api.py && \
+flock bloom/tasks/clean_positions.py --command "
+    python bloom/tasks/clean_positions.py && 
+    python bloom/tasks/create_update_excursions_segments.py"
