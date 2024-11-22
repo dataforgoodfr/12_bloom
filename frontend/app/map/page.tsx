6e5258464d8608df12c68c3aef2d55a0bedefd18
Alexandre Phiev <alexandre.phiev@gmail.com>
2024-11-22 11:28:35 +0100

    Revalidate last vessels position every hour

--- frontend/app/map/page.tsx
+++ frontend/app/map/page.tsx
@@ -1,37 +1,43 @@
+import {
+  getVessels,
+  getVesselsLatestPositions,
+} from "@/services/backend-rest-client"
+
 import LeftPanel from "@/components/core/left-panel"
 import MapControls from "@/components/core/map-controls"
 import DemoMap from "@/components/core/map/main-map"
 import PositionPreview from "@/components/core/map/position-preview"
-import { getVessels, getVesselsLatestPositions } from "@/services/backend-rest-client"
+
+export const revalidate = 3600
 
 async function fetchVessels() {
   try {
-      const response = await getVessels();
-      return response?.data;
-
-    } catch(error) {
-      console.log("An error occured while fetching vessels: " + error)
-      return [];
-    }
+    const response = await getVessels()
+    return response?.data
+  } catch (error) {
+    console.log("An error occured while fetching vessels: " + error)
+    return []
+  }
 }
 
 // TODO(CT): move this logic within a cron job
 async function fetchLatestPositions() {
   try {
-    const response = await getVesselsLatestPositions();
-    return response?.data;
-    
-  } catch(error) {
-    console.log("An error occured while fetching vessels latest positions: " + error)
-    return [];
+    const response = await getVesselsLatestPositions()
+    return response?.data
+  } catch (error) {
+    console.log(
+      "An error occured while fetching vessels latest positions: " + error
+    )
+    return []
   }
 }
-  
-export default async function MapPage() {
-  const vessels = await fetchVessels();
-  const latestPositions = await fetchLatestPositions();
 
-  // TODO: create new client component dedicated to update store 
+export default async function MapPage() {
+  const vessels = await fetchVessels()
+  const latestPositions = await fetchLatestPositions()
+
+  // TODO: create new client component dedicated to update store
   // -> then Map + LeftPanel can just use storeProvider
   return (
     <>
