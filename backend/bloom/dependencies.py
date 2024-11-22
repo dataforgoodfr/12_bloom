b7b3c4c96119d654021906815596055f357d265a
herve.le-bars <herve.le-bars@aviation-civile.gouv.fr>
2024-11-22 15:51:45 +0100

    ++ not OK

--- backend/bloom/dependencies.py
+++ backend/bloom/dependencies.py
@@ -11,7 +11,9 @@ from bloom.routers.requests import RangeHeader
 ## Reference for pagination design
 ## https://jayhawk24.hashnode.dev/how-to-implement-pagination-in-fastapi-feat-sqlalchemy
 X_API_KEY_HEADER=APIKeyHeader(name="x-key")
+from fastapi.responses import JSONResponse
 
+from fastapi.encoders import jsonable_encoder
 
 ## FastAPI endpoint decorator to manage Redis caching
 # Needs to add request:Request and nocache:bool parameters to all endpoints
@@ -33,7 +35,7 @@ def cache(func):
         nocache=True if request.query_params.get('nocache') \
                             and request.query_params.get('nocache').lower() == 'true' \
                      else False
-        cache_key=f"{request.url.path}/{request.query_params}"
+        cache_key=f"{request.url.path}/{request.query_params}#{request.headers['range'] if 'range' in request.headers else ''}"
         incache= cache_service.get(cache_key)
         #logger.debug(f"nocache: {nocache}")
         #logger.debug(f"incache: {True if incache is not None else False}")
@@ -51,6 +53,33 @@ def cache(func):
         return payload
     return wrapper
 
+
+def cache_json_response(func):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
+        start = time.time()
+        request=kwargs['request']
+        cache_service=UseCases().cache_service()
+        nocache=True if request.query_params.get('nocache') \
+                            and request.query_params.get('nocache').lower() == 'true' \
+                     else False
+        cache_key=f"{request.url.path}/{request.query_params}#{request.headers['range'] if 'range' in request.headers else ''}"
+        incache= cache_service.get(cache_key)
+        #logger.debug(f"nocache: {nocache}")
+        #logger.debug(f"incache: {True if incache is not None else False}")
+        
+        
+        if incache and not nocache:
+            #logger.debug(f"{cache_key} cached ({settings.redis_cache_expiration})s")
+            logger.debug(f"Getting response from cache")
+            payload=JSONResponse(**json.loads(incache))
+        else:
+            payload=await func(*args, **kwargs)
+            cache_service.set(cache_key, json.dumps(payload.__dict__).encode())
+            cache_service.expire(cache_key,settings.redis_cache_expiration)
+        logger.debug(f"{cache_key} elapsed Time: {time.time()-start}")
+        return payload
+    return wrapper
 
 def check_apikey(key:str):
     if key != settings.api_key :
--- backend/bloom/routers/v1/zones.py
+++ backend/bloom/routers/v1/zones.py
@@ -1,46 +1,72 @@
 from fastapi import APIRouter, Depends, Request
-from typing_extensions import Annotated
+from typing_extensions import Annotated, Any
 import json
 from bloom.container import UseCases
 from bloom.domain.zone import ZoneListView
 from bloom.dependencies import (X_API_KEY_HEADER,
                                 check_apikey,
-                                cache)
+                                cache,cache_json_response)
 from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 from bloom.routers.requests import (
     RangeHeader,
     RangeHeaderParser
 )
+from fastapi import FastAPI
+from bloom.config import settings
+from bloom.routers.requests import RangeHeader, PaginatedResult, NonPaginatedResult
 
 
 router = APIRouter()
 
+def get_cached_key(request: Request):
+    return f"{request.url.path}/{request.query_params}#range:{request.headers.get('range','')}"
+
+def get_cached_payload(request: Request):
+    cachekey=f"{request.url.path}/{request.query_params}#range:{request.headers.get('range','')}"
+    nocache=request.query_params.get('nocache',False).lower() == 'true'
+    use_cases = UseCases()
+    cache=use_cases.cache_service()
+    incache=cache.get(cachekey)
+    print(f"### TEST {incache is not None and not nocache}. incache {incache is not None} {not nocache}")
+    payload=None
+    if (incache is not None and not nocache):
+        payload=incache.decode()
+    return payload
 
 @router.get("/zones")
 # @cache
+#@cache_json_response
 async def list_zones(request: Request,
                      nocache: bool = False,
                      key: str = Depends(X_API_KEY_HEADER),
-                     range: Annotated[RangeHeader, Depends(RangeHeaderParser)] = None):
+                     range: Annotated[RangeHeader, Depends(RangeHeaderParser)] = None,
+                     cached_key: Annotated[str,Depends(get_cached_key)]=None,
+                     cached_payload: Annotated[Any, Depends(get_cached_payload)] = None):
     check_apikey(key)
-    print(f"Range:{range}")
     use_cases = UseCases()
-    zone_repository = use_cases.zone_repository()
-    db = use_cases.db()
-    with db.session() as session:
-        result = zone_repository.get_all_zones(session, range=range)
-        """payload = [ZoneListView(**z.model_dump())
-                        for z,total in zone_repository.get_all_zones(session,range=range)]"""
+    payload=None
+    if cached_payload is not None:
+        print("### Get from cache")
+        response=PaginatedResult(**json.loads(cached_payload.decode()))
+        print(response)
+    else:
+        zone_repository = use_cases.zone_repository()
+        db = use_cases.db()
+        with db.session() as session:
+            result = zone_repository.get_all_zones(session, range=range)
+            use_cases = UseCases()
+            cache=use_cases.cache_service()
+            cache.set(cached_key,json.dumps(jsonable_encoder(result)).encode())
+            cache.expire(cached_key,settings.redis_cache_expiration)
+    response = JSONResponse(content=payload,
+                            status_code=206 if range is not None else 200)
+    if result.spec is not None:
+        for s in result.spec:
+            response.headers.append(key='Content-Range',
+                                    value=f"{s.start if s.start != None else ''}-{s.end if s.end != None else ''}/{result.total}")
 
-        response = JSONResponse(content=jsonable_encoder(result.payload),
-                                status_code=206 if range is not None else 200)
-        if result.spec is not None:
-            for s in result.spec:
-                response.headers.append(key='Content-Range',
-                                        value=f"{s.start if s.start != None else ''}-{s.end if s.end != None else ''}/{result.total}")
-
-        return response
+    return response
 
 
 @router.get("/zones/summary")
