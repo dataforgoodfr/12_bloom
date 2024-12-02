d12064c1063962cd7efd006fe1bec7555ca9d787
RV <rvlebars@gmail.com>
2024-12-02 11:37:33 +0100

    feat: TaskExecution add delta column time to last task execution

--- backend/bloom/infra/repositories/repository_task_execution.py
+++ backend/bloom/infra/repositories/repository_task_execution.py
@@ -3,7 +3,7 @@ from datetime import datetime, timezone
 from bloom.infra.database import sql_model
 from sqlalchemy import select
 from sqlalchemy.dialects.postgresql import insert
-from sqlalchemy.sql.expression import update
+from sqlalchemy.sql.expression import update,asc,desc
 from sqlalchemy.orm import Session
 
 
@@ -26,9 +26,14 @@ class TaskExecutionRepository:
                 .values(active=False)
             )
         session.execute(stmt)
+        subquery_delta=select(pit-sql_model.TaskExecution.point_in_time)\
+                        .select_from(sql_model.TaskExecution)\
+                        .where(sql_model.TaskExecution.task_name==task_name)\
+                        .order_by(desc(sql_model.TaskExecution.point_in_time))\
+                        .limit(1).subquery()
         stmt = insert(sql_model.TaskExecution).values(
                     task_name=task_name,
                     point_in_time=pit,
-                    active=True,
-                    updated_at=datetime(timezone.utc))
+                    delta=subquery_delta,
+                    active=True)
         session.execute(stmt)
