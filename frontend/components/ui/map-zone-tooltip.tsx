b96188f94157d13029c5e19f2ffdea0f5734461d
Henri Chabert <henri@famille-chabert.com>
2024-12-13 12:34:08 +0100

    Applied feedbacks

---
+++ frontend/components/ui/map-zone-tooltip.tsx
@@ -0,0 +1,27 @@
+
+import { ZoneWithGeometry } from "@/types/zone"
+
+export interface MapZoneTooltipProps {
+  zoneInfo: ZoneWithGeometry
+}
+
+const MapZoneTooltip = ({ zoneInfo }: MapZoneTooltipProps) => {
+  const { name, category, sub_category } = zoneInfo
+
+  return (
+    <>
+      <div className="max-w-[288px] rounded-lg border border-gray-200 bg-white shadow dark:border-gray-700 dark:bg-gray-800">
+        <div className="bg-slate-700 p-5">
+          <h5 className="mb-1 text-xl font-bold tracking-tight text-gray-100 dark:text-white">
+            {name}
+          </h5>
+          <p className="mb-3 font-normal text-gray-200 dark:text-gray-400">
+            {category} / {sub_category}
+          </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default MapZoneTooltip
