f8eabd0404ebabc2e0932f49d19f115804a03997
marthevienne <123016211+marthevienne@users.noreply.github.com>
2024-12-13 17:52:51 +0100

    remove margin sidebar-expander

--- frontend/components/ui/custom/sidebar-expander.tsx
+++ frontend/components/ui/custom/sidebar-expander.tsx
@@ -95,7 +95,7 @@ function SidebarExpander({ disabled = false, children, className = "", opened = 
       {!disabled && (
         <div className="mt-2 flex items-start">
           <button
-            className="flex rounded-full mt-1"
+            className="flex rounded-full"
             onClick={() => setShowContent(!showContent)}
           >
             <motion.div
