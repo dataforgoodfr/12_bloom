f413dc7c05ec7505ade50a21c06880e356cb562b
Alexandre Phiev <alexandre.phiev@gmail.com>
2024-12-19 10:38:56 +0100

    fix: added missing prop to links for security

--- frontend/components/core/map/partner-credits.tsx
+++ frontend/components/core/map/partner-credits.tsx
@@ -1,30 +1,34 @@
-import Image from "next/image";
-
-
-
-
+import Image from "next/image"
 
 interface Props {
   wide: boolean
 }
 
-export default function PartnerCredits({
-  wide = false,
-}: Props) {
+export default function PartnerCredits({ wide = false }: Props) {
   return (
-    <div className={`text-xs font-bold z-50 flex justify-around items-center gap-2 ${wide ? "block" : "hidden"}`}>
+    <div
+      className={`z-50 flex items-center justify-around gap-2 text-xs font-bold ${wide ? "block" : "hidden"}`}
+    >
       <p>by</p>
-      <a href="https://bloomassociation.org" target="_blank">
+      <a
+        href="https://bloomassociation.org"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Image src="/img/bloom-logo.png" alt="Logo" width={60} height={50} />
       </a>
       <p className="text-base">&</p>
-      <a href="https://dataforgood.fr/" target="_blank">
+      <a
+        href="https://dataforgood.fr/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
         <Image
           src="/img/data-for-good-logo.png"
           alt="Logo"
           width={90}
           height={50}
-          />
+        />
       </a>
     </div>
   )
