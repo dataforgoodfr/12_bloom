ae585ded5fd0ac41c424414ecbcf39c239f367e4
marthevienne <123016211+marthevienne@users.noreply.github.com>
2026-03-04 16:31:54 +0100

    remove orchestration script

--- etl.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-
-source /home/bas/venv/bin/activate
-cd $APP_FOLDER
-
-python bloom/tasks/load_spire_data_from_api.py && \
-flock bloom/tasks/clean_positions.py --command "
-    python bloom/tasks/clean_positions.py && 
-    python bloom/tasks/create_update_excursions_segments.py"
