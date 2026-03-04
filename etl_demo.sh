964d89f70903f50c3c76e87b164c0da7045ce47b
marthevienne <123016211+marthevienne@users.noreply.github.com>
2026-03-04 16:31:25 +0100

    add local orchestration script

--- etl_demo.sh
+++ etl_demo.sh
@@ -5,7 +5,7 @@ source /Users/marthevienne/12_bloom/backend/.venv/bin/activate
 [ ! -f /Users/marthevienne/12_bloom/backend/.env ] || export $(grep -v '^#' /Users/marthevienne/12_bloom/backend/.env | xargs)
 cd ./backend/dbt_trawlwatch
 
-sleep 60
+sleep 30
 
 dbt run --select observ_spire_ais_data_retrievals
 dbt run --select itm_vessel_last_raw_position
