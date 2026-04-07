e2888137f52ee09402401dae290582bb9a6d07c3
marthevienne <123016211+marthevienne@users.noreply.github.com>
2026-04-07 10:35:44 +0200

    test: add dbt run for real-time positions

--- cron.sh
+++ cron.sh
@@ -1,9 +1,9 @@
 #! /bin/bash -l
 
 source ${APP_HOME}/backend/.venv/bin/activate
-# python ${APP_HOME}/backend/load_spire_data_from_api.py
-# python -m bloom.usecase.create_kpler_ais_messages &&\
+python -m bloom.usecase.create_kpler_ais_messages &&\
 cd ${APP_HOME}/backend/dbt_trawlwatch &&\
+dbt deps &&\
 dbt run --select observ_spire_ais_data_retrievals &&\
 dbt run --select itm_vessel_last_raw_position &&\
 dbt run --select mart_dim_vessels__last_positions
